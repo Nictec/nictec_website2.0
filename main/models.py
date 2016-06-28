3a37cb386a96ab6f4c6b253f491117bc80eed327
Nictec <nicnac9911@gmail.com>
2016-06-28 09:07:01 +0200

    models.py with the equipment model without the yes/no question.

--- main/models.py
+++ main/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class equipment(models.Model): 
+    name = models.CharField(max_length=30) 
+    storeplace = models.IntegerField() 
+    labor_choices = (  
+        ('L', 'Licht'), 
+        ('T', 'Ton'), 
+        ('R', 'rigging'),
+    ) 
+    labor = models.CharField(max_length=1, choices=labor_choices)  
+   
+    
